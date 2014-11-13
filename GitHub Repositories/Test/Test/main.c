bea9e2f8941f6b2710ceaaff4b7704d73e36e5a1
yanyan.cas@gmail.com <Yanyan@Yans-MacBook-Pro.local>
2014-11-13 16:36:11 +0800

    Initial Commit

---
+++ GitHub Repositories/Test/Test/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Test
+//
+//  Created by Yan Yan on 11/13/14.
+//  Copyright (c) 2014 YANS. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
